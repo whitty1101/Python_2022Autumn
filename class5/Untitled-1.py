378178c04cf1a9d583fbebdf64f96cffd440a79f
whitty1101 <bagelcatboy@gmail.com>
2022-11-19 12:19:18 +0800

    class6 20221119

--- class5/Untitled-1.py
+++ class5/Untitled-1.py
@@ -1,4 +1,3 @@
-'''
 from tkinter import *
 from datetime import*
 #建立視窗FRAME
@@ -22,27 +21,3 @@ mybutton.pack(pady=20)
 
 
 root.mainloop()
-'''
-'''
-from tkinter import *
-#建立視窗FRAME
-root=Tk()
-#TITLE
-root.title('class4_HW1')
-root.geometry('400x400+150+200')
-mylabel=Label(root, text="點擊按鈕次數", fg='orange', font=('garamond','25','bold'))
-mylabel.pack()
-x=0
-def clicked():
-    global x
-    x=x+1
-    label1=Label(root,text=x)
-    label1.pack()
-mybutton=Button(root,text="Click me",command=clicked)
-mybutton.pack(pady=20)
-
-
-
-
-root.mainloop()
-'''
--- class5/hw5.py
+++ class5/hw5.py
@@ -42,27 +42,28 @@ from tkinter import *
 #建立視窗FRAME
 root=Tk()
 #TITLE
-root.title('class4_HW1')
+root.title('class5_HW')
 root.geometry('400x400+150+200')
 label1=Label(root,text='三人坐沙發 綠色/灰色/黑色')
 label1.pack()
 label2=Label(root,text='NT.28,900')
 label2.pack(side='left',padx=10)
-label3=Label(root,text='0')
-AddButton=Button(root,text='+',)
+label3=Label()
+x=0
+def add():
+    global x
+    x=x+1
+    label3=Label(root,text=x)
+AddButton=Button(root,text='+',command=add)
 AddButton.pack(side='right',padx=10)
 label3.pack(side='right',padx=10)
+y=0
+def minus():
+    global y
+    y=y+1
+    label3=Label(root,text=y)
 MinusButton=Button(root,text='-',)
 MinusButton.pack(side='right',padx=10)
-
-
-
-
-
-
-
-
-
 
 
 root.mainloop()
